fa3c5ead5c5c9bdcdc28b49eb6d8548eb0bef4ca
hschwarz <hschwarz@et.esiea.fr>
2021-05-31 20:06:19 +0200

    ajout de la classe PokeApplication afin de créer l'objet context

--- app/src/main/java/com/example/app_pokemon/affichage/singleton.kt
+++ app/src/main/java/com/example/app_pokemon/affichage/singleton.kt
@@ -1,15 +1,26 @@
 package com.example.app_pokemon.affichage
 
 import com.example.app_pokemon.affichage.api.PokeApi
+import okhttp3.Cache
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.io.File
 
 class Singleton {
     companion object {
+
+        var cache: Cache = Cache(File(context.getCacheDir(), "responses"), 10 * 1024 * 1024) // 10Mib
+        val okhttpClient: OkHttpClient = OkHttpClient().newBuilder()
+            .build()
+
              val pokeApi: PokeApi = Retrofit.Builder()
                 .baseUrl("https://pokeapi.co/api/v2/")
                 .addConverterFactory(GsonConverterFactory.create())
+                 .client(okhttpClient)
                 .build()
                 .create(PokeApi::class.java)
+
+
         }
     }
